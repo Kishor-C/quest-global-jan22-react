d929c1611b9ca3e231a4cf25b94bbf94d5fe9f61
Kishor <kishor1124@gmail.com>
2022-01-21 19:53:14 +0530

    react with axios

--- React Apps/my-first-app/src/App.js
+++ React Apps/my-first-app/src/App.js
@@ -1,27 +1,42 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
-import Form from './Form'
-function CounterComponent() {
-  // initializing the state & creating a function that updates the state
-  const [counter, setCounter] = useState(1);
-  function addCounter() {
-    setCounter(counter + 1);
+
+function User() {
+  const [user, setUser] = useState([]);
+  let url = 'https://jsonplaceholder.typicode.com/users';
+  
+  function handleClick() {
+    axios.get(url).then((response) => {
+      setUser(response.data);
+    });
   }
-  return (<div>
-    <h2>Simple counter state from function component</h2>
-    <p>Counter: {counter}</p>
-    <button onClick = {addCounter} className = 'btn btn-primary'>Add Counter</button>
+  return(<div>
+    <h2>User information from JSON placeholder</h2>
+    <button onClick = {handleClick}>Refresh</button>
+    <table className = 'table table-stripted'>
+      <thead>
+        <tr>
+          <th>Id</th><th>Name</th><th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {user.map((item, index) => <tr key = {index}>
+          <td>{item.id}</td>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+        </tr>)}
+      </tbody>
+    </table>
+   
   </div>)
 }
 
 function App() {
   return (<div className = 'container-fluid'>
     <div className = 'row'>
-      <div className = 'col-4'>
-        <Form />
-      </div>
-      <div className = 'col-4'>
-        <CounterComponent />
+      <div className = 'col'>
+        <User />
       </div>
     </div>
     </div>
